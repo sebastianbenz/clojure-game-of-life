6bd3442f9a5fbe5d62cff6a6edacf79b0d74ef90
Sebastian Benz <sebastian.benz@gmail.com>
2010-11-21 14:24:43 +0100

    Made simulator configurable.

--- src/simulator/field.clj
+++ src/simulator/field.clj
@@ -5,55 +5,63 @@
     (java.awt.event ActionListener )
     (java.awt Dimension)))
 
-(defn draw-box [g x y color]
- (let [x (int (* x 10))
-       y (int (* y 10))]
-  (.setColor g color)
-  (.fillRect g x y 10 10)
-  (.setColor g Color/BLACK)
-  (.drawRect g x y 10 10)))
+(defn draw-box 
+  [g cellsize x y color]
+  (let [x (int (* x cellsize))
+        y (int (* y cellsize))]
+    (.setColor g color)
+    (.fillRect g x y cellsize cellsize)
+    (.setColor g Color/BLACK)
+    (.drawRect g x y cellsize cellsize)))
 
 
-(defn field-panel [game-engine cells width height]
-  (let [aCells (atom cells)
+(defn field-panel 
+  [engine cells options]
+  (let [columns (options :columns)
+        rows (options :rows)
+        cellsize (options :cellsize)
+        aCells (atom cells)
         panel (proxy [JPanel ActionListener] []
-    (paintComponent [g] 
-      (let [oldState @aCells
-            newState (game-engine oldState)]
-        
-      (loop [x 0]
-        (if (= x 50)
-          nil
-          (do
-            (loop [y 0]
-              (if (= y 50)
-                nil
-                (do
-                   (if (contains? @aCells [x y])
-                     (draw-box g x y Color/BLACK)
-                     (draw-box g x y Color/WHITE))
-                  (recur (inc y)))) )
+                (paintComponent [g] 
+                  (let [oldState @aCells
+                        newState (engine oldState)]
+                    (loop [x 0]
+                      (if (= x columns)
+                        nil
+                        (do
+                          (loop [y 0]
+                            (if (= y rows)
+                              nil
+                              (do
+                                (if (contains? @aCells [x y])
+                                  (draw-box g cellsize x y Color/BLACK)
+                                  (draw-box g cellsize x y Color/WHITE))
+                                (recur (inc y)))) )
             (recur (inc x)))) )))
-    (actionPerformed [e]
-      (swap! aCells game-engine)
-      (.repaint this)))]
+                (actionPerformed [e]
+                  (swap! aCells engine)
+                  (.repaint this)))]
     (doto panel
-      (.setPreferredSize (Dimension. width height)))))
+      (.setPreferredSize (Dimension. (* columns cellsize)  (* rows cellsize))))))
 
 
-(defn field-frame [panel]
+(defn field-frame 
+  [panel]
   (doto (JFrame. "Game of Life")
-;    (.setDefaultCloseOperation (JFrame/EXIT_ON_CLOSE))
+    (.setDefaultCloseOperation (JFrame/EXIT_ON_CLOSE))
     (.add panel)
     .pack
     .show))
 
-(defn run-game [game-engine cells]
-  (let [width 500 
-        height 500
-        panel (field-panel game-engine cells width height)
-        frame (field-frame panel)
-        timer (Timer. 500 panel)]
-  (.start timer)))
+(defn run-game 
+  ([engine cells]  
+    (run-game engine cells  
+      {:columns 50 :rows 50 :speed 500 :cellsize 10}))
+  
+  ([engine cells options] 
+    (let [panel (field-panel engine cells options)
+          frame (field-frame panel)
+          timer (Timer. (options :speed) panel)]
+      (.start timer))))
 
 
--- src/simulator/run.clj
+++ src/simulator/run.clj
@@ -1,6 +1,9 @@
 (ns simulator.run)
 (use 'simulator.field )
 (use 'engine.game )
+
 (def cells #{[0 0][1 1][2 2]})
+(def options  {:columns 192 :rows 100 :speed 300 :cellsize 10})
+
 (run-game game cells)
 
